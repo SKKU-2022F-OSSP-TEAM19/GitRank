f2b03436f0d09d1e667fdbe8e46fdd29cc2ffb29
NaInho <gnih1226@gmail.com>
2022-11-26 20:47:33 +0900

    profile update

--- frontend/myprofile.js
+++ frontend/myprofile.js
@@ -1,10 +1,12 @@
 let user;
 let profile;
 window.onload = function () {
+    let span_id = document.getElementById("user_id");
     let span_git = document.getElementById("user_github");
     let span_itr = document.getElementById("user_interests");
     let span_con = document.getElementById("user_contact");
     let span_com = document.getElementById("user_comment");
+    let span_skills = document.getElementById("user_skills");
     let span_contribution = document.getElementById("contribution");
     let img = document.getElementById("my_img");
 
@@ -22,15 +24,19 @@ window.onload = function () {
             .then(json => {
                 //alert("Hi");
                 console.log(json);
+                span_id.innerHTML = json.ID;
                 span_git.innerHTML = json.GITHUB;
                 span_itr.innerHTML = json.INTERESTS;
+                span_skills.innerHTML = json.SKILLS;
                 if (json.CONTACT) {
                     span_con.innerHTML = json.CONTACT;
+                    span_con.href = "mailto:" + json.CONTACT;
                 }
                 if (json.COMMENT) {
                     span_com.innerHTML = json.COMMENT;
                 }
                 span_contribution.innerHTML = json.SKILLS;
+                span_contribution.src = "https://ghchart.rshah.org/" + json.GITHUB;
                 if (json.IMG !== "none") {
                     img.src = json.IMG;
                 }
--- frontend/profile.js
+++ frontend/profile.js
@@ -1,22 +1,27 @@
 let user;
 let profile;
 window.onload = function () {
+    let span_id = document.getElementById("user_id");
     let span_git = document.getElementById("user_github");
     let span_itr = document.getElementById("user_interests");
     let span_con = document.getElementById("user_contact");
     let span_com = document.getElementById("user_comment");
+    let span_skills = document.getElementById("user_skills");
     let span_contribution = document.getElementById("contribution");
-    let img = document.getElementById("user_img");
+    let img = document.getElementById("my_img");
 
     if (localStorage.getItem("profile")) {
         user = JSON.parse(JSON.stringify(localStorage.getItem("profile")));
         user = JSON.parse(user);
         console.log(user);
+        span_id.innerHTML = user.ID;
         span_git.innerHTML = user.GITHUB;
         span_itr.innerHTML = user.INTERESTS;
         span_con.innerHTML = user.CONTACT;
+        span_con.href = "mailto:" + user.CONTACT;
         span_com.innerHTML = user.COMMENT;
-        span_contribution.innerHTML = user.SKILLS;
+        span_skills.innerHTML = user.SKILLS;
+        span_contribution.src = "https://ghchart.rshah.org/" + user.GITHUB;
         if (user.IMG !== "none") {
             img.src = user.IMG;
         }
