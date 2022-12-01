110f6f4660ce333779092f3b6cede2ab77e27a09
stbaker517 <stbaker517@naver.com>
2022-12-02 00:22:02 +0900

    gitscore api fixed it's no longer use outer api. It directly parse html from github page

--- backend/gitrank/routes/index.js
+++ backend/gitrank/routes/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const e = require('express');
 var express = require('express');
 var router = express.Router();
@@ -269,27 +270,33 @@ router.get('/score/:username',async (req,res)=>{
           let now=new Date()
 
           let root=parser.parse(data);
-          root=root.querySelector("svg");
-          root.childNodes.forEach((e)=>{
-            let arr = e.rawAttrs.split(" ");
-            if(arr.length===7){
+          //root=root.querySelector("svg");
+          console.log(data)
+        //   root.childNodes.forEach((e)=>{
+        //     let arr = e.rawAttrs.split(" ");
+        //     if(arr.length===7){
 
                 
-                let date=arr[2].match(dataRegex)[0].split("-");
-                let year=Number.parseInt(date[0])
-                let month=Number.parseInt(date[1])
+        //         let date=arr[2].match(dataRegex)[0].split("-");
+        //         let year=Number.parseInt(date[0])
+        //         let month=Number.parseInt(date[1])
 
-                let score=Number.parseInt(arr[1].match(scoreRegex)[0])
-                score=score*(now.getFullYear()===year?12-now.getMonth()+month:1)
+        //         let score=Number.parseInt(arr[1].match(scoreRegex)[0])
+        //         score=score*(now.getFullYear()===year?12-now.getMonth()+month:1)
 
                 
-                retScore+=score
+        //         retScore+=score
 
-            }
-        })
+        //     }
+        // })
+        userPageInfo[0].SCORE=retScore;
+        DB_profile=DB_profile.filter(e=>e.ID!==username);
+        DB_profile.push(userPageInfo[0]);
+        dbFuncs.saveDatas(false,true,DB_users,DB_profile);
         res.status(200).json({
           gitscore:retScore
         });
+
         })
     });
 
@@ -307,6 +314,7 @@ router.get('/score/:username',async (req,res)=>{
 
 
 })
+
 
 router.get('/user/signin/:username/:password',(req,res)=>{
   let username=req.params.username;
@@ -371,6 +379,73 @@ router.get('/user/signingin/:username',(req,res)=>{
       result:"error:no such user sginined"
     })
   }
+})
+
+router.get('/user/contribution/score/:username',(req,res)=>{
+
+  let username=req.params.username;
+
+  let userPageInfo=DB_profile.filter(e=>e.ID===username);
+  if(userPageInfo.length===0){
+    res.status(404).json({
+      result:"error:no such user"
+    })
+  }
+
+  https.get('https://github.com/'+userPageInfo[0].GITHUB,rres=>{
+    let data=""
+    rres.on("data",d=>{
+        data+=d;
+    })
+    rres.on('end',()=>{
+        let root=parser.parse(data);
+        root=root.querySelector(".js-calendar-graph-svg");
+        //console.log(root)
+        //root=root.toString()
+        let retScore = 500;
+        root=root.querySelectorAll(".ContributionCalendar-day");
+        let date=0;
+        root.forEach((e)=>{
+          let values=Object.values(e._attrs);
+          let count=Number(values[7]);
+          //console.log(count)
+          retScore=retScore+(count*(parseInt(++date/37)))
+          
+//          Object.keys
+
+        })
+        //   root.childNodes.forEach((e)=>{
+        //     let arr = e.rawAttrs.split(" ");
+        //     if(arr.length===7){
+
+                
+        //         let date=arr[2].match(dataRegex)[0].split("-");
+        //         let year=Number.parseInt(date[0])
+        //         let month=Number.parseInt(date[1])
+
+        //         let score=Number.parseInt(arr[1].match(scoreRegex)[0])
+        //         score=score*(now.getFullYear()===year?12-now.getMonth()+month:1)
+
+                
+        //         retScore+=score
+
+        //     }
+        // })
+
+        //console.log(retScore);
+        //console.log(root.querySelector(".js-calendar-graph-svg"))
+
+        
+        userPageInfo[0].SCORE=retScore;
+        DB_profile=DB_profile.filter(e=>e.ID!==username);
+        DB_profile.push(userPageInfo[0]);
+        dbFuncs.saveDatas(false,true,DB_users,DB_profile);
+        res.status(200).json({
+          gitscore:retScore
+        });
+    })
+})
+  
 })
 module.exports = router;
 
